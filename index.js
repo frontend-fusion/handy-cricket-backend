39768327529d83dc3c9e0d6faec3d8cde3c6b410
frontend-fusion <frontendfusions@gmail.com>
2024-07-02 23:07:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-const app = express()
-import { createServer } from "http";
-import { Server } from "socket.io";
+const app = express();
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { customAlphabet } from 'nanoid';
 
 const httpServer = createServer(app);
@@ -14,28 +14,20 @@ const io = new Server(httpServer, {
 const activeRooms = {};
 let prevPlayerTotalScore = 0;
 
-// const _dirname = path.resolve();
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(_dirname, '/frontend/chatty/build')));
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(_dirname, 'frontend', 'chatty', 'build', 'index.html'));
-//     });
-// }
-
 io.on("connection", (socket) => {
     socket.on('create room', (userName) => {
         const roomId = generateRoomId();
         activeRooms[roomId] = {
             totalScore: 0,
             isBothPlayed: false,
-            users: [{ userName: userName, score: 0, id: socket.id, makeMove: false}]
+            users: [{ userName: userName, score: 0, id: socket.id, makeMove: false }]
         }
         socket.join(roomId);
         socket.emit('room created', roomId);
     });
 
     socket.on('join room', (userName, roomId) => {
-         roomId = roomId.toLowerCase();
+        roomId = roomId.toLowerCase();
         if (!activeRooms[roomId]) {
             socket.emit('room not found');
             return;
@@ -64,6 +56,20 @@ io.on("connection", (socket) => {
             socket.emit('room not found');
         }
     });
+    
+    socket.on('play again', (roomId) => {
+        console.log(activeRooms[roomId])
+        activeRooms[roomId].totalScore = 0;
+        activeRooms[roomId].users[0].score = 0;
+        activeRooms[roomId].users[1].score = 0;
+        activeRooms[roomId].users[0].makeMove = false;
+        activeRooms[roomId].users[1].makeMove = false;
+        activeRooms[roomId].isBothPlayed = false;
+        io.to(roomId).emit('restartMatch', activeRooms);
+    })
+    
+    
+
     socket.on('player move', (roomId, move) => {
         const index = activeRooms[roomId].users.findIndex(user => user.id === socket.id);
         activeRooms[roomId].users[index].makeMove = true;
@@ -73,50 +79,43 @@ io.on("connection", (socket) => {
             return;
         }
 
-        if (!activeRooms[roomId]) {
-            socket.emit('room not found');
-            return;
-        }
-
-        else {        
+        else {
             if (activeRooms[roomId].users[0].score === activeRooms[roomId].users[1].score && activeRooms[roomId].users[0].score) {
                 activeRooms[roomId].users[0].score = 0;
                 activeRooms[roomId].users[1].score = 0;
                 activeRooms[roomId].users[0].makeMove = false;
-                activeRooms[roomId].users[1].makeMove = false
-                if(!activeRooms[roomId].isBothPlayed) {
-                [activeRooms[roomId].users[0], activeRooms[roomId].users[1]] = [activeRooms[roomId].users[1], activeRooms[roomId].users[0]];
-                activeRooms[roomId].isBothPlayed = true;
-                prevPlayerTotalScore = activeRooms[roomId].totalScore;
-                let batterScore = activeRooms[roomId].totalScore;
-                activeRooms[roomId].totalScore = 0;
-                io.to(roomId).emit('bowled out' , activeRooms[roomId].users[1].userName, activeRooms[roomId].users[0].userName, activeRooms, batterScore);
+                activeRooms[roomId].users[1].makeMove = false;
+                if (!activeRooms[roomId].isBothPlayed) {
+                    [activeRooms[roomId].users[0], activeRooms[roomId].users[1]] = [activeRooms[roomId].users[1], activeRooms[roomId].users[0]];
+                    activeRooms[roomId].isBothPlayed = true;
+                    prevPlayerTotalScore = activeRooms[roomId].totalScore;
+                    let batterScore = activeRooms[roomId].totalScore;
+                    activeRooms[roomId].totalScore = 0;
+                    io.to(roomId).emit('bowled out', activeRooms[roomId].users[1].userName, activeRooms[roomId].users[0].userName, activeRooms, batterScore);
                 }
-                else {   
-                const winner = +(prevPlayerTotalScore) > +(activeRooms[roomId].totalScore) ? activeRooms[roomId].users[1].userName : activeRooms[roomId].users[0].userName;
-                const draw = prevPlayerTotalScore === activeRooms[roomId].totalScore;
-                activeRooms[roomId].totalScore = 0;
-                console.log(activeRooms[roomId]);
-                io.to(roomId).emit('out', winner, draw, activeRooms);
+                else {
+                    const winner = +(prevPlayerTotalScore) > +(activeRooms[roomId].totalScore) ? activeRooms[roomId].users[1].userName : activeRooms[roomId].users[0].userName;
+                    const draw = prevPlayerTotalScore === activeRooms[roomId].totalScore;
+                    activeRooms[roomId].totalScore = 0;
+                    console.log(activeRooms[roomId]);
+                    io.to(roomId).emit('out', winner, draw, activeRooms, roomId);
                 }
             }
-            else if(activeRooms[roomId].isBothPlayed && prevPlayerTotalScore < activeRooms[roomId].totalScore+ +(activeRooms[roomId].users[0].score)) {
-                io.to(roomId).emit('user2 won match', activeRooms[roomId].users[0].userName);
-                console.log(activeRooms[roomId]);
+            else if (activeRooms[roomId].isBothPlayed && prevPlayerTotalScore < activeRooms[roomId].totalScore + +(activeRooms[roomId].users[0].score)) {
+                io.to(roomId).emit('user2 won match', activeRooms[roomId].users[0].userName, roomId);
+                    console.log(activeRooms[roomId]);
             }
             else {
-             console.log(activeRooms[roomId]);
-            activeRooms[roomId].totalScore = activeRooms[roomId].totalScore + +(activeRooms[roomId].users[0].score);
-            activeRooms[roomId].users[0].makeMove = false;
-            activeRooms[roomId].users[1].makeMove = false;
-            io.to(roomId).emit('score updated', activeRooms);
+                console.log(activeRooms[roomId]);
+                activeRooms[roomId].totalScore = activeRooms[roomId].totalScore + +(activeRooms[roomId].users[0].score);
+                activeRooms[roomId].users[0].makeMove = false;
+                activeRooms[roomId].users[1].makeMove = false;
+                io.to(roomId).emit('score updated', activeRooms);
             }
         }
-
     });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
         Object.keys(activeRooms).forEach((roomId) => {
             const index = activeRooms[roomId].users.findIndex(user => user.id === socket.id);
             if (index !== -1) {
